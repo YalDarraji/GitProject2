e9458892a5938f877368d5b6348567f1c0d9c36b
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-29 10:32:06 -0400

    subtract ()

--- src/DemoDay2.java
+++ src/DemoDay2.java
@@ -1,13 +1,18 @@
 
 public class DemoDay2 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Test");
-		
-	}//add mehthod
-	public static void add (int a , int b) {
-		
+
+	}// add mehthod
+
+	public static void add(int a, int b) {
+
 		System.out.println(a + b);
+	}
+
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
 	}
 }
