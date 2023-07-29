5c342ba658b8c1ec2f7320f1533c00928b653168
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-29 10:48:43 -0400

    Update DemoDay2.java

--- src/DemoDay2.java
+++ src/DemoDay2.java
@@ -19,4 +19,10 @@ public class DemoDay2 {
 	public static void divid(int a, int b) {
 		System.out.println(a / b);
 }
+	// add mehthod
+
+	public static void add(int a, int b, int c) {
+
+		System.out.println(a + b + c);
+}
 }
