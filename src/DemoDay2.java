869bd72d51cbe2d64ae1b9f3366f5db0a8c18dd4
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-29 10:30:21 -0400

    implemented add()

--- src/DemoDay2.java
+++ src/DemoDay2.java
@@ -1,4 +1,13 @@
 
 public class DemoDay2 {
-
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test");
+		
+	}//add mehthod
+	public static void add (int a , int b) {
+		
+		System.out.println(a + b);
+	}
 }
