e803d25567e0f2e4b7233cc83f60df7897d78336
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-29 11:05:51 -0400

    Update DemoDay3.java

--- src/Day3/DemoDay3.java
+++ src/Day3/DemoDay3.java
@@ -5,5 +5,9 @@ public class DemoDay3 {
 		
 		System.out.println("Yasir");
 	}
+	public static void country (String a , String b){
+		String a = "USA";
+		String b = "VA"
+			system.out.println ( a , b ) ;
 
 }
